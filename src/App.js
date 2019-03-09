e0b4416ef040e8d32ab4e87fa61730a080632593
campbellbg <campbell.bg@gmail.com>
2019-03-09 14:06:33 +1100

    made a change to app.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>
-            This is change that I am making in order to test volumes
+            This is me hanging out in AWS Elastic Beanstalk
           </p>
           <a
             className="App-link"
